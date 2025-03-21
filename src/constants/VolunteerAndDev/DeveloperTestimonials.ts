22d2411db71347bae4e7ffdc338899567d3981fd
mayur-driod <settymayurk@gmail.com>
2025-03-21 16:57:11 +0530

    fixed dev testimonials

---
+++ src/constants/VolunteerAndDev/DeveloperTestimonials.ts
@@ -0,0 +1,14 @@
+export const developertestimonials = [
+  {
+    name: 'Riya Lohia',
+    username: 'Riya Lohia, GSoC 2018 Student',
+    body: 'I would like to thank my mentors Walter Bender and Devin Ulibarri for their constant guidance and motivation. A big thanks to Sugar Labs community and Google Summer of Code (GSoC) for providing such a wonderful opportunity for a wonderful opportunity for learning and helping me to be an active member of Open Source  Community.',
+    img: 'https://avatar.vercel.sh/jack',
+  },
+  {
+    name: 'Sourabha G.',
+    username: 'Sourabha G., GSoC 2021 Student',
+    body: "This summer was super fun and filled with learning! I enjoyed testing over 60+ activities with kids to get first-hand feedback which had a positive impact in the way they approach problems. The fact that these activities are used by kids as a tool of learning motivates me to contribute more and more. I'm going to continue to contribute and be a part of Sugar Labs.",
+    img: 'https://avatar.vercel.sh/jill',
+  },
+];
