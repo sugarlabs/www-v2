a160af91d610185138c074d981d2e9a6f4f6e1b2
mayur-driod <settymayurk@gmail.com>
2025-03-21 16:37:24 +0530

    added real data

--- src/constants/Testimonials.ts
+++ src/constants/Testimonials.ts
@@ -1,33 +1,14 @@
-export const testimonials = [
-  {
-    name: 'Student at Soham Shubham Science',
-    username: 'Soham Shubham Science',
-    body: 'Music Blocks is a programming environment for children interested in music and graphics. It expands upon Turtle Blocks by adding a collection of features relating to pitch and rhythm.',
-    img: 'https://subhamgroupofinstitutions.com/wp-content/uploads/2022/08/Subham_logo-1-removebg-preview.png',
-  },
-  {
-    name: 'Richard Hermann',
-    username:
-      'Richard Hermann, PhD, Prof. of Music Theory and Composition, University of New Mexico',
-    body: 'I think Music Blocks will inspire many young folks to become programmers, mathematicians, composers, and music theorists. It will also provide important experiences to other young folk who pursue other fields. You are to be congratulated on your efforts.',
-    img: 'https://www.richardhermann.net/_Media/rhmh-tentrock_med.jpeg',
-  },
+export const developertestimonials = [
   {
     name: 'Riya Lohia',
-    username: 'Riya Lohia GSoc 2018 Student',
-    body: 'I would like to thank my mentors Walter Bender and Devin Ulibarri for their constant guidance and motivation...',
-    img: 'https://avatar.vercel.sh/james',
+    username: 'Riya Lohia, GSoC 2018 Student',
+    body: 'I would like to thank my mentors Walter Bender and Devin Ulibarri for their constant guidance and motivation. A big thanks to Sugar Labs community and Google Summer of Code (GSoC) for providing such a wonderful opportunity for a wonderful opportunity for learning and helping me to be an active member of Open Source  Community.',
+    img: 'https://avatar.vercel.sh/jack',
   },
   {
     name: 'Sourabha G.',
-    username: 'Sourabha G. GSoC 2021 Student',
-    body: 'This summer was super fun and filled with learning! I enjoyed testing over 60+ activities with kids to get first-hand feedback which had a positive impact in the way they approach problems...',
-    img: 'https://avatar.vercel.sh/james',
-  },
-  {
-    name: 'LinuxInsider',
-    username: 'LinuxInsider',
-    body: 'Fedora-Based Sugar on a Stick Is One Sweet Desktop.',
-    img: 'https://avatar.vercel.sh/james',
-  },
+    username: 'Sourabha G., GSoC 2021 Student',
+    body: "This summer was super fun and filled with learning! I enjoyed testing over 60+ activities with kids to get first-hand feedback which had a positive impact in the way they approach problems. The fact that these activities are used by kids as a tool of learning motivates me to contribute more and more. I'm going to continue to contribute and be a part of Sugar Labs.",
+    img: 'https://avatar.vercel.sh/jill',
+  }
 ];
