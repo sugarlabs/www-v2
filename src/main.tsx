5a4893c33b6b65539a79d190bae147b8edb82d2c
Muhammad Haroon <haroongondal347@gmail.com>
2025-04-28 18:56:05 +0500

    fix website rendering on OLPC Operating System

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from '@/App';
 import '@/styles/globals.css';
 import '@/utils/copy-code';
 
+const browserType = window.navigator.userAgent;
+
+if (browserType === "OLPC") {
+  window.location.href = "redirect to old website";
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
