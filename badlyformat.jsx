8ac3ae5189ee728375379ff49242554b82fc3018
Shashi sah <sah.shashi2003@gmail.com>
2025-04-29 00:15:56 +0530

    Add badly formatted component with initial implementation

---
+++ badlyformat.jsx
@@ -0,0 +1,10 @@
+function   BadlyFormattedComponent(  ){
+    const x='not properly formatted'
+return(
+        <div>
+    {x}
+        </div>
+    )
+}
+
+export default     BadlyFormattedComponent
