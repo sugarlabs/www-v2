6fc26f8f3c1af87bf587bf40e7c50fc4b9b4f473
ling66 <lingasreeja86@gmail.com>
2025-07-07 18:53:15 +0530

    Change the "Donate Now", "Subscribe" button cursor to pointer. And made the "Join the volunteer" button to clickable without the entire row

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -48,7 +48,7 @@ const Donation: React.FC = () => {
           {/* Donate Now Button */}
           <motion.button
             onClick={handleDonateClick}
-            className="mt-6 px-6 py-3 bg-[#fbd04d] text-black text-2xl font-bold rounded-full shadow-md hover:bg-yellow-500 transition duration-300 uppercase"
+            className="mt-6 px-6 py-3 bg-[#fbd04d] text-black text-2xl font-bold rounded-full shadow-md hover:bg-yellow-500 cursor-pointer transition duration-300 uppercase"
             variants={bounce}
             whileHover="hover"
             whileTap="tap"
@@ -72,7 +72,6 @@ const Donation: React.FC = () => {
 
       {/* Newsletter and Volunteer Section*/}
       <div className="relative z-10 w-full mt-12 text-center">
-        <Link to="/volunteer">
           <motion.div
             className="flex justify-center items-center"
             variants={slideInBottom}
@@ -90,89 +89,90 @@ const Donation: React.FC = () => {
               >
                 <i className="fas fa-arrow-right text-2xl" />
               </motion.div>
-
-              <motion.div
-                className="bg-white border border-gray-300 rounded-full px-4 py-2 flex items-center shadow-lg"
-                whileHover={{ scale: 1.05 }}
-                transition={{ type: 'spring', stiffness: 400, damping: 10 }}
-              >
-                {volunteerImages.map((volunteer, index) => (
-                  <motion.img
-                    key={volunteer.id}
-                    alt={volunteer.alt}
-                    className="w-8 h-8 rounded-full border-2 border-white -ml-2"
-                    src={volunteer.src}
-                    initial={{ opacity: 0, x: -20 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ delay: 0.05 * index }}
-                  />
-                ))}
-                <span className="ml-4 text-sm">Join the volunteer</span>
-                <motion.span
-                  className="ml-2 bg-blue-500 text-white text-xs rounded-full px-2 py-1"
-                  initial={{ scale: 0 }}
-                  animate={{
-                    scale: 1,
-                    transition: {
-                      type: 'spring',
-                      stiffness: 400,
-                      damping: 10,
-                      delay: 0.3,
-                    },
-                  }}
+              <Link to="/volunteer">
+                <motion.div
+                  className="bg-white border border-gray-300 rounded-full px-4 py-2 flex items-center shadow-lg"
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ type: 'spring', stiffness: 400, damping: 10 }}
                 >
-                  +1000
-                </motion.span>
-              </motion.div>
+                  {volunteerImages.map((volunteer, index) => (
+                    <motion.img
+                      key={volunteer.id}
+                      alt={volunteer.alt}
+                      className="w-8 h-8 rounded-full border-2 border-white -ml-2"
+                      src={volunteer.src}
+                      initial={{ opacity: 0, x: -20 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ delay: 0.05 * index }}
+                    />
+                  ))}
+                  <span className="ml-4 text-sm">Join the volunteer</span>
+                  <motion.span
+                    className="ml-2 bg-blue-500 text-white text-xs rounded-full px-2 py-1"
+                    initial={{ scale: 0 }}
+                    animate={{
+                      scale: 1,
+                      transition: {
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                        delay: 0.3,
+                      },
+                    }}
+                  >
+                    +1000
+                  </motion.span>
+                </motion.div>
+              </Link>
+
             </div>
           </motion.div>
-        </Link>
 
-        {/* Newsletter Subscription Section */}
-        <motion.div
-          className="mt-16"
-          variants={staggerContainer}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.2 }}
-        >
-          <motion.p className="text-lg" variants={fadeIn}>
-            Join us to empower young learners across the globe. The simplest way
-            to get involved is to join our newsletter.
-          </motion.p>
-
+          {/* Newsletter Subscription Section */}
           <motion.div
-            className="mt-4 flex justify-center items-center"
-            variants={slideInRight}
+            className="mt-16"
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.2 }}
           >
-            <motion.form
-              action="https://buttondown.com/api/emails/embed-subscribe/sugarlabs"
-              method="post"
-              onSubmit={() => setTimeout(() => setEmail(''), 500)}
-              className="flex"
+            <motion.p className="text-lg" variants={fadeIn}>
+              Join us to empower young learners across the globe. The simplest way
+              to get involved is to join our newsletter.
+            </motion.p>
+
+            <motion.div
+              className="mt-4 flex justify-center items-center"
+              variants={slideInRight}
             >
-              <input
-                className="px-4 py-2 border border-gray-300 rounded-l-full focus:outline-none"
-                placeholder="Enter your email"
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <input value="1" type="hidden" name="embed" />
-              <motion.button
-                className="px-6 py-2 bg-red-500 text-white font-bold rounded-r-full shadow-lg hover:bg-red-600 transition duration-300"
-                variants={buttonAnimation}
-                whileHover="whileHover"
-                whileTap="whileTap"
-                type="submit"
+              <motion.form
+                action="https://buttondown.com/api/emails/embed-subscribe/sugarlabs"
+                method="post"
+                onSubmit={() => setTimeout(() => setEmail(''), 500)}
+                className="flex"
               >
-                SUBSCRIBE
-              </motion.button>
-            </motion.form>
+                <input
+                  className="px-4 py-2 border border-gray-300 rounded-l-full focus:outline-none"
+                  placeholder="Enter your email"
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <input value="1" type="hidden" name="embed" />
+                <motion.button
+                  className="px-6 py-2 bg-red-500 text-white font-bold rounded-r-full shadow-lg hover:bg-red-600 cursor-pointer transition duration-300"
+                  variants={buttonAnimation}
+                  whileHover="whileHover"
+                  whileTap="whileTap"
+                  type="submit"
+                >
+                  SUBSCRIBE
+                </motion.button>
+              </motion.form>
+            </motion.div>
           </motion.div>
-        </motion.div>
       </div>
     </section>
   );
