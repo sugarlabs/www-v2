0f457810519184f9a1bc8df28b0142ca5e319f43
Mkn0021 <nayeem21122002@gmail.com>
2025-12-18 23:58:06 +0600

    fix(join-volunteer): fix the contrast of header title in light mode

--- src/components/JoinToggle.tsx
+++ src/components/JoinToggle.tsx
@@ -23,10 +23,9 @@ const JoinToggle = () => {
         <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start">
           {/* Volunteer (Active or Clickable) */}
           <motion.h2
-            className="text-4xl md:text-[5rem] font-extralight pb-1 md:pb-2 cursor-pointer transition-colors"
+            className={`text-4xl md:text-[5rem] font-extralight pb-1 md:pb-2 cursor-pointer transition-colors ${isVolunteerPage ? 'text-red-500' : 'text-gray-600 dark:text-gray-300'}`}
             initial={{ opacity: 0.6, scale: 0.95 }}
             animate={{
-              color: isVolunteerPage ? '#EF4444' : '#D1D5DB',
               opacity: isVolunteerPage ? 1 : 0.7,
               scale: isVolunteerPage ? 1.1 : 1,
             }}
@@ -37,14 +36,13 @@ const JoinToggle = () => {
           </motion.h2>
 
           {/* Divider Line */}
-          <div className="border-t border-gray-300 w-3/4 md:w-4/5"></div>
+          <div className="border-t border-gray-600 dark:border-gray-300 w-3/4 md:w-4/5"></div>
 
           {/* Development (Active or Clickable) */}
           <motion.h2
-            className="text-4xl md:text-[5rem] font-extralight pt-1 md:pt-2 cursor-pointer transition-colors"
+            className={`text-4xl md:text-[5rem] font-extralight pt-1 md:pt-2 cursor-pointer transition-colors ${isDevPage ? 'text-red-500' : 'text-gray-600 dark:text-gray-300'}`}
             initial={{ opacity: 0.6, scale: 0.95 }}
             animate={{
-              color: isDevPage ? '#EF4444' : '#D1D5DB',
               opacity: isDevPage ? 1 : 0.7,
               scale: isDevPage ? 1.1 : 1,
             }}
