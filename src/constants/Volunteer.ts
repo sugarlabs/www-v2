e8403db84f51a520a957eaf7400a17cc9f01cc43
Supritha-gurazala <171582752+Supritha-gurazala@users.noreply.github.com>
2026-01-21 20:38:09 +0530

    chore: ignore node_modules

--- src/constants/Volunteer.ts
+++
@@ -1,9 +0,0 @@
-import volunteer1 from '/assets/Volunteer/volunteer-1.webp';
-import volunteer2 from '/assets/Volunteer/volunteer-2.webp';
-import volunteer3 from '/assets/Volunteer/volunteer-3.webp';
-
-export const volunteerImages = [
-  { id: 1, alt: 'Volunteer 1', src: volunteer1 },
-  { id: 2, alt: 'Volunteer 2', src: volunteer2 },
-  { id: 3, alt: 'Volunteer 3', src: volunteer3 },
-];
